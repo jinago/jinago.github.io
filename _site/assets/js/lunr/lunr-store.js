3637e7d145331c130f2b6194e9add8fbd957b54d
Apple <>
2020-08-09 20:44:35 +0700

    ddd

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,8 +12,8 @@ var store = [{
         "teaser": null
       },{
         "title": "Sony a7SIII – 5 năm đợi chờ đã được đền đáp",
-        "excerpt":"Tối qua theo giờ Việt Nam, Sony chính thức công bố chiếc máy ảnh full-frame thuộc dòng chuyên quay phim của họ, Sony a7SIII. Đây là thành quả kết tinh sau gần 5 năm kể từ người tiền nhiệm Sony a7SII Theo một nghiên cứu thị trường, tính đến năm...","categories": ["jekyll","lap-trinh"],
+        "excerpt":"Tối qua theo giờ Việt Nam, Sony chính thức công bố chiếc máy ảnh full-frame thuộc dòng chuyên quay phim của họ, Sony a7SIII. Đây là thành quả kết tinh sau gần 5 năm kể từ người tiền nhiệm Sony a7SII Theo một nghiên cứu thị trường, tính đến năm...","categories": ["lap-trinh"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/lap-trinh/deo-hieu/",
+        "url": "http://localhost:4000/lap-trinh/deo-hieu/",
         "teaser": null
       }]
